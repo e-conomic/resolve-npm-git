581f925bf38c395ae9030727bf75fe2758d5ca09
Mathias Buus <mathiasbuus@gmail.com>
2015-01-07 16:21:34 +0100

    fix replace bug

--- bin.js
+++ bin.js
@@ -6,9 +6,9 @@ var fs = require('fs')
 var minimist = require('minimist')
 
 var argv = minimist(process.argv.slice(2), {alias:{file:'f', replace:'r'}})
-var file = argv.file || require(path.join(process.cwd(), 'package.json'))
+var file = argv.file || 'package.json'
 
-resolve(file, function(err, pkg) {
+resolve(require(path.resolve(file)), function(err, pkg) {
   if (err) throw err
   if (argv.replace) fs.writeFileSync(file, JSON.stringify(pkg, null, 2))
   else console.log(JSON.stringify(pkg, null, 2))
